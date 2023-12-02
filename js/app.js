6d4d1152ce549f14d76c3e2f93c272d5356596fd
Jose Santiago <santiagojos@hotmail.com>
2023-12-02 14:16:15 -0600

    Caprtuando el DOM

---
+++ js/app.js
@@ -0,0 +1,39 @@
+//capturamos el DOM 
+
+const pantalla = document.getElementById(inOut);// input de pantalla
+
+// capturamso a los botones
+
+const n0 = document.getElementById("0");
+const n1 = document.getElementById("1");
+const n2 = document.getElementById("2");
+const n3 = document.getElementById("3");
+const n4 = document.getElementById("4");
+const n5 = document.getElementById("5");
+const n6 = document.getElementById("6");
+const n7 = document.getElementById("7");
+const n8 = document.getElementById("8");
+const n9 = document.getElementById("9");
+
+// capturamos botones de signos e igual
+
+const btnIgual = document.getElementById("=");
+const btnPunto = document.getElementById(".");
+const btnLimpiar = document.getElementById("c");
+const btnSuma = document.getElementById("+");
+const btnResta = document.getElementById("-");
+const btnMultipli = document.getElementById("x");
+const btnDivi = document.getElementById("/");
+
+
+
+
+
+
+
+
+
+
+
+
+
